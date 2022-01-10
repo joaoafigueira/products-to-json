357a8797acf37f3c1518cc3b0622b14625cb3940
João Victor <juvito9813@gmail.com>
2022-01-10 09:27:56 -0300

    feat: initial commit

---
+++ src/main/java/br/com/joao/productsjson/ProductsJsonApplication.java
@@ -0,0 +1,13 @@
+package br.com.joao.productsjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joao/productsjson/ProductsJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.productsjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
