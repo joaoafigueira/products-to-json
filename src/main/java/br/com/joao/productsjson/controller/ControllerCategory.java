537ddec5035ababd422b35b2aa4ca3afd61a6104
João Victor <juvito9813@gmail.com>
2022-01-21 09:59:52 -0300

    feat/delete-product:allows-you-to-delete-products

--- src/main/java/br/com/joao/productsjson/controller/ControllerCategory.java
+++ src/main/java/br/com/joao/productsjson/controller/ControllerCategory.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -104,6 +105,21 @@ public class ControllerCategory {
 
 		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This product doesnt exist in our database");
 
+	}
+
+	@DeleteMapping("deleteProduct/{id}")
+	@Transactional
+	@CacheEvict(value = { "availableCategories", "availableProducts" }, allEntries = true)
+	public ResponseEntity<?> removeProduct(@PathVariable Long id) {
+
+		Optional<Product> productIdExists = repositoryProduct.findById(id);
+
+		if (productIdExists.isPresent()) {
+
+			repositoryProduct.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This product doesnt exist in our database");
 	}
 
 }
