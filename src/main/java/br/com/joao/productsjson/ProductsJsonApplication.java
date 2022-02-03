377ff806643bcd8bb82190068cc2d1eddce772d1
João Victor <juvito9813@gmail.com>
2022-02-03 08:52:02 -0300

    docs/products-to-json:implements-swagger-to-see-the-documentatin-about-the-api

--- src/main/java/br/com/joao/productsjson/ProductsJsonApplication.java
+++ src/main/java/br/com/joao/productsjson/ProductsJsonApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableCaching
 @EnableSpringDataWebSupport
+@EnableSwagger2
 public class ProductsJsonApplication {
 
 	public static void main(String[] args) {
